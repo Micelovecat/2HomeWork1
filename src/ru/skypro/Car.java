e412efba0927d5a10c7f0e52b7447b6886ae980f
Micelovecat <flora_bux@mail.ru>
2022-01-26 17:02:54 +0300

    Second commit

---
+++ src/ru/skypro/Car.java
@@ -0,0 +1,8 @@
+package ru.skypro;
+
+public class Car extends WheeledTransport{
+
+    public Car(String modelName) {
+      super(modelName, 4);
+    }
+}
---
+++ src/ru/skypro/Truck.java
@@ -0,0 +1,18 @@
+package ru.skypro;
+
+public class Truck extends WheeledTransport{
+
+    public Truck(String modelName, int wheelsCount) {super(modelName, wheelsCount);}
+
+    public Truck(String modelName) {super(modelName, 6);}
+
+    public void checkTrailer() {
+        System.out.println("Проверяем прицеп");
+    }
+
+    @Override
+    public void service() {
+        super.service();
+        checkTrailer();
+    }
+}
